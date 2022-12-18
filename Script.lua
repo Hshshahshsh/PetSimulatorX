ce150c3776454c9cb692b4cf5a0d477e69b3efc7
Hshshahshsh <110442121+Hshshahshsh@users.noreply.github.com>
2022-12-18 18:39:50 +0800

    Create Script.lua

---
+++ Script.lua
@@ -0,0 +1,44 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+getgenv().AuthKey="HUGE_TrialKey"
+getgenv().LoadSettings={
+    UseSeperateSaves=false,
+    DisableStartupPrompt=true,
+    DisableGamepassCheck=false,
+}
+loadstring(game:HttpGet("https://hugegames.io/script"))()
